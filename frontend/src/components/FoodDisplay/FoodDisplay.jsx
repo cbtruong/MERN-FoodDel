8a240f5f2d2e3fc4d15f404346784321824f5bc1
cbtruong <batruong12122002@gmail.com>
2025-08-21 16:23:55 +0700

    feat(ui): create exploreMenu and footer components

---
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -0,0 +1,28 @@
+import React, { useContext, useEffect } from 'react'
+import {StoreContext} from '../../context/StoreContext';
+import './FoodDisplay.css';
+import FoodItem from '../FoodItem/FoodItem';
+
+const FoodDisplay = ({category}) => {
+  const {food_list} = useContext(StoreContext);
+  useEffect(()=>{
+    console.log(food_list);
+  },[])
+  return (
+    <div className='food-display' id='food-display'>
+      <h2>Top dishes near you</h2>
+      <div className="food-display-list">
+        {
+          food_list.map((item,index)=>{
+            if (category === 'All' || category === item.category )
+            return (
+              <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/>
+            )
+          })
+        }
+      </div>
+    </div>
+  )
+}
+
+export default FoodDisplay
---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import './Footer.css';
+import { assets } from '../../assets/assets';
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <div className="footer-content">
+            <div className="footer-content-left">
+                <img src={assets.logo} alt="" />
+                <p>abaskjdhajsdjkasdjas .............</p>
+                <div className="footer-social-icons">
+                    <img  src={assets.facebook_icon} alt="" />
+                    <img  src={assets.twitter_icon} alt="" />
+                    <img  src={assets.linkedin_icon} alt="" />
+                </div>
+            </div>
+            <div className="footer-content-center">
+                    <h2>COMPANY</h2>
+                    <ul>
+                        <li>Home</li>
+                        <li>About us</li>
+                        <li>Delivery</li>
+                        <li>Privacy policy</li>
+                    </ul>
+            </div>
+            <div className="footer-content-right">
+                <h2>GET IN TOUCH</h2>
+                <ui>
+                    <li>+84 123456789</li>
+                    <li>contact@gmail.com</li>
+                </ui>
+            </div>
+        </div>
+        <p className="footer-copyright">Copyrigth 2024 @ CB.Trường - All right reserved</p>
+    </div>
+  )
+}
+
+export default Footer
