faf162073029314bb80e0011a55203dae6d4c9a6
cbtruong <batruong12122002@gmail.com>
2025-08-22 07:29:45 +0700

    feat(ui): create loginPopup and cart component

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -18,16 +18,24 @@ export const StoreContextProvider =(props)=>{
     }
 
     
-    useEffect(()=>{
-        console.log(food_list);
-    },[])
+    const getTotalCartAmount=()=>{
+        let totalAmount=0;
+        for(const item in cartItems){
+            if (cartItems[item]>0){
+                let itemInfo = food_list.find((product)=>product._id === item._id );
+                totalAmount +=itemInfo.price * cartItems[item];
+            }
+        }
+        return totalAmount;
+    }
 
     const contextValue={
         food_list,
         cartItems,
         setCartItems,
         addToCart,
-        removeFormCart
+        removeFormCart,
+        getTotalCartAmount
     }
     return(
         <StoreContext.Provider value={contextValue}>
