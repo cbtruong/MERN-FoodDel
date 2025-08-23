cb9e1c1811bec08fe1c99bd5fd85acb63596c8b6
cbtruong <batruong12122002@gmail.com>
2025-08-24 03:55:01 +0700

    feat(be): create add food functionality

---
+++ backend/routes/foodRouter.js
@@ -0,0 +1,29 @@
+import express from 'express';
+
+import { addFood } from '../controllers/foodController.js';
+import multer from 'multer';
+
+const foodRouter = express.Router();
+
+
+
+// Image engine upload
+const storage= multer.diskStorage({
+    // determine storage location image
+    destination:"uploads",
+    // name the file image
+    // cb: callback function to report result
+    filename:(req,file,cb)=>{
+        return cb(null,`${Date.now()}${file.originalname}`)
+    }
+})
+// create middleware to upload file image
+const upload = multer({storage:storage});
+
+foodRouter.post("/add",upload.single("image"),addFood)
+
+export default foodRouter;
+
+
+
+
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import { connectDB } from './config/db.js';
-import foodRouter from './routes/foodRoute.js';
+import foodRouter from './routes/foodRouter.js';
 
 
 // app config
@@ -17,6 +17,7 @@ connectDB();
 
 // api endpoints
 app.use("/api/food",foodRouter);
+app.use("/images",express.static("uploads"));
 
 app.get("/",(req,res)=>{
     res.send("Hello!!!");
